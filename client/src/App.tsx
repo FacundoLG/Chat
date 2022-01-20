43886ca2a2276bc7d30c0b2812c2a7e7ec930a83
FacundoLG <facu-judoca@hotmail.com.ar>
2022-01-20 16:51:02 -0300

    room chat fully working, now i will add some tyles

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,24 +1,77 @@
 import './App.css';
 import {io} from "socket.io-client"
-import { useEffect, useState } from 'react';
+import { FC, useEffect, useState } from 'react';
 
+interface Message {
+  username: string,
+  message: string,
+  time: Date
+}
 
-function App() {
-  const [message,setMessage] = useState<string>()
-  useEffect(()=> {
-    const ioClient = io("http://localhost:3030", {transports: ["websocket"]})
-   
-    ioClient.on("message",(iomessage) => {
-      setMessage(iomessage)
+const App:FC = () => {
+  const [room,setRoom] = useState<string>("global")
+  const [ioClient,setIoClient] = useState<any>(null)
+  const [messages,setMessages] = useState<Array<Message>>([])
+  const [chatInputValue,setChatInputValue] = useState<string>("")
+  
+  useEffect(() => {
+    setMessages([])
+    setIoClient(io("http://localhost:3030", {transports: ["websocket"]}))
+  },[room])
+  console.log(ioClient)
+  
+  useEffect(() => {
+    if(!ioClient || !room) return
+
+    ioClient.on("connect",() => {
+      ioClient.emit("joinRoom",room) 
+      ioClient.on("resivedMessage", (newMessage:Message) => {
+        setMessages(messages => [...messages, newMessage])
+      })
+      
     })
-
     return () => {
       ioClient.disconnect()
     }
-  },[])
+  },[ioClient,room])
+  
+  const sendMessage = () => {
+    const userMessage:Message = {
+      username: ioClient.id,
+      message: chatInputValue,
+      time: new Date()
+    }
+   if(chatInputValue?.length > 0) ioClient.emit("sendMessage", userMessage)
+  }
+  // console.log(messages[0].time)
   return (
     <div className="App">
-        <h1>{message || "hi"}</h1>
+        <label htmlFor="room">Room</label>
+        <input type="text" id='room' />
+        <button onClick={() => {
+          const roomText = document.querySelector<HTMLInputElement>("#room")?.value
+          console.log(roomText)
+          setRoom(roomText || "global")
+        }}>Enter</button>
+        <span>Current Room: </span>
+        <div className='Messages'>
+          {messages?.map(({username,message,time}) => (
+            <div className='Message'>
+              <p className='time'>{time.toString()}</p>
+              <p><span className={username}>{username + ": "}</span> {message}</p>
+            </div>
+          ))}
+        </div>
+          <input id="chatInput" className='chatInput' type="text"
+          value={chatInputValue}
+          onChange={(e) => {
+            setChatInputValue(e.target.value)
+          }} />
+            
+            <button onClick={() => { 
+              sendMessage(); 
+              setChatInputValue("")    
+            }}>Send Message</button>
     </div>
   );
 }
